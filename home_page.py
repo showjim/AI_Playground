00812766aa61200528c10569ed0d084693f716d7
Chao Zhou <zhouchao486@gmail.com>
2023-06-29 18:45:54 +0800

    1. CCaht/Trans: support streaming; 2. change to streamlit chat;

--- home_page.py
+++ home_page.py
@@ -1,7 +1,7 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
 
-__version__ = "Beta V0.0.15"
+__version__ = "Beta V0.0.16"
 
 st.set_page_config(
     page_title="Jerry's AI Playground",
