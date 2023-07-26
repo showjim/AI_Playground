54eb9aa760c6627f2675503aed5b4457052bb607
chao zhou <zhouchao486@gmail.com>
2023-07-26 22:11:19 +0800

    modify EVAL

--- pages/7_🔍_ChatBotEval.py
+++ pages/7_🔍_ChatBotEval.py
@@ -7,15 +7,15 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTex
 work_path = os.path.abspath('.')
 def setup_env():
     # Load OpenAI key
-    if os.path.exists(os.path.join("../", "key.txt")):
-        shutil.copyfile(os.path.join("../", "key.txt"), "../.env")
+    if os.path.exists("key.txt"):
+        shutil.copyfile("key.txt", ".env")
         load_dotenv()
     else:
         print("key.txt with OpenAI API is required")
 
     # Load config values
-    if os.path.exists(os.path.join(r'../config.json')):
-        with open(r'../config.json') as config_file:
+    if os.path.exists(os.path.join(r'config.json')):
+        with open(r'config.json') as config_file:
             config_details = json.load(config_file)
 
         # Setting up the embedding model
@@ -59,39 +59,40 @@ def define_embedding():
 
 def main():
     # Initial
+    setup_env()
     if "evalreloadflag" not in st.session_state:
         st.session_state["evalreloadflag"] = True
 
     # Setup Side Bar
     with st.sidebar:
         # 1. Model
-        aa_llm_model = st.radio(label="LLM Model",
+        aa_llm_model = st.radio(label="`LLM Model`",
                                 options=["gpt-35-turbo", "gpt-35-turbo-16k"],
                                 index=0,
                                 on_change=set_reload_setting_flag)
         # 2. Split
-        aa_eval_q = st.slider(label="Number of eval questions",
+        aa_eval_q = st.slider(label="`Number of eval questions`",
                               min_value=1,
                               max_value=10,
                               value=5,
                               on_change=set_reload_setting_flag)
-        aa_chunk_size = st.slider(label="Choose chunk size for splitting",
+        aa_chunk_size = st.slider(label="`Choose chunk size for splitting`",
                                   min_value=500,
                                   max_value=2000,
                                   value=1000,
                                   on_change=set_reload_setting_flag)
-        aa_overlap_size = st.slider(label="Choose overlap for splitting",
+        aa_overlap_size = st.slider(label="`Choose overlap for splitting`",
                                     min_value=0,
                                     max_value=100,
                                     value=200,
                                     on_change=set_reload_setting_flag)
-        aa_split_methods = st.radio(label="Split method",
+        aa_split_methods = st.radio(label="`Split method`",
                                     options=["RecursiveTextSplitter", "CharacterTextSplitter"],
                                     index=0,
                                     on_change=set_reload_setting_flag)
 
         # 3. Retriver
-        aa_retriver = st.radio(label="Choose retriever",
+        aa_retriver = st.radio(label="`Choose retriever`",
                                options=["Azure Cognitive Search", "OpenAI", "SVM"],
                                index=0,
                                on_change=set_reload_setting_flag)
@@ -100,19 +101,19 @@ def main():
                                         on_change=set_reload_setting_flag)
 
         # 4. Embedding
-        aa_embedding_method = st.radio(label="Choose embeddings",
+        aa_embedding_method = st.radio(label="`Choose embeddings`",
                                        options=["Azure Cognitive Search", "OpenAI"],
                                        index=0,
                                        on_change=set_reload_setting_flag)
 
     if st.session_state["evalreloadflag"] == True:
-        st.session_state["FileChat"].initial_llm(aa_llm_model, 2048, 0.2)
+        st.session_state["EvalChatModel"] = define_llm(aa_llm_model)
         st.session_state["evalreloadflag"] = False
 
     # Main
     st.header("`Demo auto-evaluator`")
     file_paths = st.file_uploader("1.Upload document files to generate QAs",
-                                  type=["pdf", "txt", "pptx", "docx", "html"],
+                                  type=["pdf"],
                                   accept_multiple_files=True)
 
     if st.button("Upload"):
