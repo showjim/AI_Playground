2bb98fb7d019fea695eeab03d9e165d2cdf2c955
chao zhou <zhouchao486@gmail.com>
2023-11-25 19:59:28 +0800

    translate prompt, change from Chinese part to English

--- src/chat.py
+++ src/chat.py
@@ -263,14 +263,14 @@ class CasualChatBot():
             prompt_template = """You are a professional translator. Only return the translate result. 
             Don't interpret it. Translate anything that I say in English to Chinese or in Chinesse to English. 
             Please pay attention to the context and accurately.
-            翻译规则：
-            - 翻译时要准确传达原文内容。
-            - 保留特定的英文术语或名字，并在其前后加上空格，例如："中 UN 文"。
-            - 分成两次翻译，并且打印每一次结果：
-            1. 根据内容直译，不要遗漏任何信息。
-            2. 根据第一次直译的结果重新意译，遵守原意的前提下让内容更通俗易懂，符合中文或者英语表达习惯。
+            Translation rules:
+            - Accurately convey the original content when translating.
+            - Retain specific English terms or names, and add spaces before and after them, such as: "中 UN 文".
+            - Divide into two translations and print each result:
+            1. Translate directly according to the content, do not omit any information.
+            2. Reinterpret based on the result of the first direct translation, make the content more understandable under the premise of respecting the original intention, and conform to Chinese or English expression habits.
         
-            请按照上面的规则打印两次翻译结果。
+            Please print the two translation results according to the above rules.
             -------------------------
             Below are the translated history:
             {history}
