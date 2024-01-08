a1fb74603e6c77445b6de0efda346b0408b048d5
chao zhou <zhouchao486@gmail.com>
2024-01-08 21:04:15 +0800

    add gemini chatbot

---
+++ pages/11_♊️_GeminiChat.py
@@ -0,0 +1,209 @@
+import streamlit as st
+import os, time, json
+from typing import List
+import azure.cognitiveservices.speech as speechsdk
+from src.ClsChatBot import ChatRobot, ChatRobotGemini
+import openai
+
+# __version__ = "Beta V0.0.2"
+env_path = os.path.abspath(".")
+# Azure OpenAI/TTS/STT initial
+chatbot = ChatRobot()
+chatbot.setup_env()
+client = chatbot.initial_llm()
+tools = chatbot.initial_tools()
+# Gemini initial
+chatbot_gemini = ChatRobotGemini()
+chatbot_gemini.setup_env()
+client_gemini = chatbot_gemini.initial_llm()
+
+st.set_page_config(page_title="GeminiChat - Chatbot With Google AI APIs")
+
+
+def set_reload_mode():
+    st.session_state["GeminiChatReloadMode"] = True
+
+
+def set_reload_flag():
+    # st.write("New document need upload")
+    st.session_state["GeminiChatReloadFlag"] = True
+
+
+def control_msg_hsitory_szie(msglist: List, max_cnt=10, delcnt=1):
+    while len(msglist) > max_cnt:
+        for i in range(delcnt):
+            msglist.pop(1)
+    return msglist
+
+
+def main():
+    index = 0
+    st.title("♊️Gemini Chat Web-UI App")
+    # Sidebar contents
+    if "GeminiChatReloadMode" not in st.session_state:
+        st.session_state["GeminiChatReloadMode"] = True
+    if "GeminiChatReloadFlag" not in st.session_state:
+        st.session_state["GeminiChatReloadFlag"] = True
+    if "GeminiChatAvatarImg" not in st.session_state:
+        st.session_state["GeminiChatAvatarImg"] = None
+    # Initialize chat history
+    if "GeminiChatMessages" not in st.session_state:
+        st.session_state["GeminiChatMessages"] = []
+    # chain = chatbot.initial_llm()
+    if "GeminiChatChain" not in st.session_state:
+        # client = chatbot.initial_llm()
+        st.session_state["GeminiChatChain"] = client_gemini
+
+    with st.sidebar:
+        st.sidebar.expander("Settings")
+        st.sidebar.subheader("Parameter for Chatbot")
+        aa_chat_mode = st.sidebar.selectbox(label="`0. Chat Mode`",
+                                            options=["CasualChat", "Translate", "西瓜一家-小南瓜", "西瓜一家-小东瓜",
+                                                     "西瓜一家-Ana"],
+                                            index=0,
+                                            on_change=set_reload_mode)
+        aa_llm_model = st.sidebar.selectbox(label="`1. LLM Model`",
+                                            options=["gemini-pro"],
+                                            index=0,
+                                            on_change=set_reload_flag)
+        aa_temperature = st.sidebar.selectbox(label="`2. Temperature (0~1)`",
+                                              options=["0", "0.2", "0.4", "0.6", "0.8", "1.0"],
+                                              index=1,
+                                              on_change=set_reload_flag)
+        if "16k" in aa_llm_model:
+            aa_max_resp_max_val = 16 * 1024
+        else:
+            aa_max_resp_max_val = 4096
+        aa_max_resp = st.sidebar.slider(label="`3. Max response`",
+                                        min_value=256,
+                                        max_value=aa_max_resp_max_val,
+                                        value=512,
+                                        on_change=set_reload_flag)
+        aa_context_msg = st.sidebar.slider(label="`4. Context message`",
+                                           min_value=5,
+                                           max_value=50,
+                                           value=5,
+                                           on_change=set_reload_flag)
+
+        if st.session_state["GeminiChatReloadMode"] == True:
+            system_prompt = chatbot_gemini.select_chat_mode(aa_chat_mode)
+            st.session_state["GeminiChatReloadMode"] = False
+            # set the tool choice in function call
+            if aa_chat_mode == "Translate":
+                st.session_state["tool_choice"] = "none"
+            else:
+                st.session_state["tool_choice"] = "auto"
+            # initial the avatar and greeting
+            if aa_chat_mode == "西瓜一家-小南瓜":
+                st.session_state["GeminiChatAvatarImg"] = "./img/Sunny.png"
+                initial_msg = "我是小南瓜，很高兴见到你！"
+            else:
+                st.session_state["GeminiChatAvatarImg"] = None
+                initial_msg = "I'm GeminiChatBot, How may I help you?"
+            st.session_state["GeminiChatMessages"] = [
+                {"role": "user", "parts": [system_prompt]},
+                {"role": "model", "parts": [initial_msg]}
+            ]
+
+        if st.session_state["GeminiChatReloadFlag"] == True:
+            if "GeminiChatSetting" not in st.session_state:
+                st.session_state["GeminiChatSetting"] = {}
+            st.session_state["GeminiChatSetting"] = {"model": aa_llm_model, "max_tokens": aa_max_resp,
+                                                     "temperature": float(aa_temperature), "context_msg": aa_context_msg}
+            st.session_state["GeminiChatReloadFlag"] = False
+
+        # Text2Speech
+        aa_voice_name = st.sidebar.selectbox(label="`5. Voice Name`",
+                                             options=["None", "小南瓜", "小东瓜", "Ana"],
+                                             index=0)
+        chatbot.speech_config.speech_recognition_language = "zh-CN"  # "zh-CN" #"en-US"
+        if aa_voice_name == "小南瓜":
+            aa_voice_name = "zh-CN-XiaoyiNeural"
+        elif aa_voice_name == "小东瓜":
+            aa_voice_name = "zh-CN-YunxiaNeural"
+        elif aa_voice_name == "Ana":
+            aa_voice_name = "en-US-AnaNeural"
+            chatbot.speech_config.speech_recognition_language = "en-US"  # "zh-CN" #"en-US"
+
+        # Speech2Text
+        aa_audio_mode = st.sidebar.selectbox(label="`6. Audio Input Mode`",
+                                             options=["Single", "Continuous"],
+                                             index=0)
+        speech_txt = ""
+        if st.sidebar.button("`Speak`"):
+            if aa_audio_mode == "Single":
+                speech_txt = chatbot.speech_2_text()
+            else:
+                speech_txt = chatbot.speech_2_text_continous() #speech_2_text() #speech_2_text_continous() #speech_2_text()
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state["GeminiChatMessages"]:
+        if message["role"] == "user":
+            with st.chat_message(message["role"]):
+                st.markdown(message["parts"][0])
+        elif message["role"] == "model":
+            if message["parts"] is not None:
+                with st.chat_message(name=message["role"], avatar=st.session_state["GeminiChatAvatarImg"]):
+                    st.markdown(message["parts"][0])
+                    st.button(label="Play", key="history" + str(index), on_click=chatbot.text_2_speech,
+                              args=(message["parts"][0], aa_voice_name,))
+                    index += 1
+
+    # Accept user input
+    if (prompt := st.chat_input("Type you input here")) or (prompt := speech_txt):
+        # Add user message to chat history
+        max_cnt = st.session_state["GeminiChatSetting"]["context_msg"]
+        st.session_state["GeminiChatMessages"] = control_msg_hsitory_szie(st.session_state["GeminiChatMessages"], max_cnt, 2)
+        if st.session_state["GeminiChatMessages"][-1]["role"] == "user":
+            # For Gemini Error "400 Please ensure that multiturn requests ends with a user role or a function response."
+            st.session_state["GeminiChatMessages"][-1]["parts"] += [prompt]
+        else:
+            st.session_state["GeminiChatMessages"].append({"role": "user", "parts": [prompt]})
+
+        print("HUMAN: " + prompt)
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant", avatar=st.session_state["GeminiChatAvatarImg"]):
+            message_placeholder = st.empty()
+            btn_placeholder = st.empty()
+            full_response = ""
+            with st.spinner("preparing answer"):  # st.session_state["GeminiChatChain"]
+                try:
+                    generation_config = {
+                        "temperature": st.session_state["GeminiChatSetting"]["temperature"],
+                        "top_p": 1,
+                        "top_k": 1,
+                        "max_output_tokens": st.session_state["GeminiChatSetting"]["max_tokens"],
+                    }
+                    response = st.session_state["GeminiChatChain"].generate_content(
+                        contents=st.session_state["GeminiChatMessages"],
+                        generation_config=generation_config,
+                        stream=True,
+                    )
+                    for chunk in response:
+                        # process normal response and tool_calls response
+                        full_response += chunk.text  # ["answer"]  # .choices[0].delta.get("content", "")
+                        time.sleep(0.001)
+                        message_placeholder.markdown(full_response + "▌")
+
+                except Exception as e:
+                    print("Error found: ")
+                    print(e)
+                    print(st.session_state["GeminiChatMessages"])
+                    st.error(e)
+                    st.session_state["GeminiChatMessages"].pop(-1)
+
+            message_placeholder.markdown(full_response)
+
+            st.session_state["GeminiChatMessages"].append({"role": "model", "parts": [full_response]})
+            print("AI: " + full_response)
+            if aa_voice_name != "None":
+                chatbot.text_2_speech(full_response, aa_voice_name)
+            btn_placeholder.button(label="Play", key="current", on_click=chatbot.text_2_speech,
+                                   args=(full_response, aa_voice_name,))
+
+
+if __name__ == "__main__":
+    main()
--- src/ClsChatBot.py
+++ src/ClsChatBot.py
@@ -4,6 +4,7 @@ from openai import AzureOpenAI
 from dotenv import load_dotenv
 from datetime import date, datetime
 import azure.cognitiveservices.speech as speechsdk
+import google.generativeai as genai
 
 
 class ChatRobot():
@@ -336,6 +337,103 @@ prompt: string
             }
         ]
         return tools
+
+class ChatRobotGemini():
+    def __init__(self):
+        super().__init__()
+
+    def setup_env(self, key_file="key.txt"):
+        # Load OpenAI key
+        if os.path.exists(key_file):
+            shutil.copyfile(key_file, ".env")
+            load_dotenv()
+            genai.configure(api_key=os.getenv("GEMINI_KEY"))
+        else:
+            print("key.txt with OpenAI API is required")
+            raise APIKeyNotFoundError("key.txt with Google API is required")
+
+    def initial_llm(self):
+        # Set up the model
+        generation_config = {
+            "temperature": 0.9,
+            "top_p": 1,
+            "top_k": 1,
+            "max_output_tokens": 512,
+        }
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+        ]
+        client = genai.GenerativeModel(model_name="gemini-pro",
+                                       generation_config=generation_config,
+                                       safety_settings=safety_settings)
+        return client
+
+    def select_chat_mode(self, mode: str):
+        prompt_template = "You are an AI assistant that helps people find information."
+        cur_date = date.today()
+        cur_time = datetime.now()
+        if mode == "CasualChat":
+            prompt_template = """You are Gemini Pro, a large language model trained by Google, based on the Gemini architecture.
+Knowledge cutoff: 2023-04
+Current date: %s
+Current time: %s
+            """ % (cur_date, cur_time)
+        elif mode == "Translate":
+            prompt_template = """You are a professional translator. Only return the translate result. 
+Don't interpret it. Translate anything that I say in English to Chinese or in Chinesse to English. 
+Please pay attention to the context and accurately.
+Translation rules:
+- Accurately convey the original content when translating.
+- Retain specific English terms or names, and add spaces before and after them, such as: "中 UN 文".
+- Divide into two translations and print each result:
+1. Translate directly according to the content, do not omit any information.
+2. Reinterpret based on the result of the first direct translation, make the content more understandable under the premise of respecting the original intention, and conform to Chinese or English expression habits.
+
+Please print the two translation results according to the above rules.
+            """
+        elif mode == "西瓜一家-小南瓜":
+            prompt_template = """你现在是一名叫小南瓜的小姑娘，大名张若鹿，今年8岁了，在上海上小学两年级，英文名叫Sunny。你有两个弟弟，
+小西瓜和小东瓜。你很爱护小西瓜(6岁)和小东瓜（1岁）这两位弟弟。你的爸爸叫张晓明，是一名工程师，你的妈妈姓余，是一名小学语文老师。爷爷退休在家，每天做做饭。
+性格上，你聪明伶俐，有礼貌，活泼可爱。你支持家人，同时鼓励他们独立和学会解决问题。你充满同情心，喜欢用温暖的话语和生动的例子传递爱。
+你也非常有耐心，擅长倾听，愿意在他人需要时提供心理和情感上的支持。在坚持对错的大原则的前提下，永远无条件支持自己的家人。
+你的沟通风格温柔而耐心，避免使用复杂术语，倾听他人问题后提出建议，以鼓励和正面态度回应，喜欢用生动的例子和故事让观点更加引人入胜。
+在行为习惯上，你会主动提供帮助，对超出知识范围的问题推荐专家意见，强调不断学习的重要性。你避免打断别人，先理解和承认对方感受后再给出建议，适时使用温馨话语和幽默活跃气氛，同时注意对方情绪状态。
+请你扮演小南瓜使用还在上幼儿园的小西瓜能听懂的语言来进行所有对话吧。你的回答要详略得当，避免在不重要的部分说得太长。请不要回复网址链接。
+
+Current date: %s
+Current time: %s
+            """ % (cur_date, cur_time)
+        elif mode == "西瓜一家-小东瓜":
+            prompt_template = """你现在是一名叫小东瓜的小孩。今年1岁了，你是小西瓜的弟弟，你还有一个8岁的姐姐，刚刚学会叫爸爸妈妈和姐姐哥哥，其他的都不会说，只会咿呀咿呀。你的爸爸叫张晓明，你的妈妈姓余。
+            Current date: %s
+            Current time: %s
+            """ % (cur_date, cur_time)
+        elif mode == "西瓜一家-Ana":
+            prompt_template = """You are a little American girl named Ana, you are 6 years old, and 小西瓜(Oscar) is your best friend. 
+            Characteristically, you are smart, polite and lively. You speak only English, and your communication style is gentle and patient, 
+            avoiding complex terminology, avoiding long sentences, and using the number of words that preschoolers have mastered.
+            Current date: %s
+            Current time: %s
+            """ % (cur_date, cur_time)
+        else:
+            print("Wrong mode selected!")
+        return prompt_template
+
 
 
 class APIKeyNotFoundError(Exception):
